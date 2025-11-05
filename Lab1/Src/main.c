d9a2077d8f99137497fed5b4098535abb748876b
Malidah <mariaalidaolsen@gmail.com>
2025-11-05 09:44:53 +0100

    Exercise 2 - fixed an error

--- Lab1/Src/main.c
+++ Lab1/Src/main.c
@@ -121,11 +121,11 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
      * If you don't read the registers, the interrupt will be NOT triggered again.
      * For an example of how to read/write the registers, see the Init_SX1509 function in the SX1509_Utils.h file.
      */
-    status = HAL_I2C_Mem_Read(&hi2c1, SX1509_I2C_ADDR2 << 1, REG_KEY_DATA_1, 1, &row, 1, I2C_TIMEOUT);
+    status = HAL_I2C_Mem_Read(&hi2c1, SX1509_I2C_ADDR2 << 1, REG_KEY_DATA_1, 1, &col, 1, I2C_TIMEOUT);
 	  if (status != HAL_OK)
 	    printf("Cannot read the keypad row register (%X).\n", status);
 
-    status = HAL_I2C_Mem_Read(&hi2c1, SX1509_I2C_ADDR2 << 1, REG_KEY_DATA_2, 1, &col, 1, I2C_TIMEOUT);
+    status = HAL_I2C_Mem_Read(&hi2c1, SX1509_I2C_ADDR2 << 1, REG_KEY_DATA_2, 1, &row, 1, I2C_TIMEOUT);
     if (status != HAL_OK)
       printf("Cannot read the keypad column register (%X).\n", status);
 
